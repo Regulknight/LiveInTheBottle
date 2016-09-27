8d0ee14ffd0b5b7553ab2907579b95fd2375d669
zelh <regulknight@gmail.com>
2016-09-27 21:55:00 +0400

    Add login page

--- test/main.py
+++ test/main.py
@@ -1,5 +1,5 @@
-from bottle import route, run
-
+from bottle import route, run, static_file, post, request
+import random
 
 @route('/hello')
 def hello():
@@ -10,4 +10,21 @@ def hello():
 def leonnash():
     return "Леонид помнит сюжет не написанных книг"
 
-run(host='localhost', port=8080, debug=True)
+
+@route('/login')
+def login():
+    return static_file("login.html", ".")
+
+
+@post('/login')
+def do_login():
+    username = request.forms.get('login')
+    password = request.forms.get('password')
+    r = random.randint(0, 10)
+    if r == 5:
+        return "Login Success"
+    else:
+        return "Login Fail"
+
+
+run(host='192.168.43.196', port=8080, debug=True)
